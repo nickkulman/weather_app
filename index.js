5da82e380b82dc611a521bf4f470288d82261591
Nick Kulman <nickkulman@gmail.com>
2021-04-08 09:59:57 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+import {AppRegistry} from 'react-native';
+import WeatherProject from './src/WeatherProject';
+import { name as appName } from "./app.json";
+
+
+AppRegistry.registerComponent(appName, () => WeatherProject);
+
+
